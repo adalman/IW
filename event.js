ed0d66ce17235ab0f3df7c9b50cae00f2a7d106f
Ally Dalman <adalman@princeton.edu>
2020-04-15 20:57:13 -0400

    formatting again

--- event.js
+++ event.js
@@ -1,22 +1,32 @@
 // Execute the inject.js in a tab and call a method,
 // passing the result to a callback function.
-function injectedMethod (tab, method, callback) {
-    chrome.tabs.executeScript(tab.id, { file: 'inject.js' }, function(){
-      chrome.tabs.sendMessage(tab.id, { method: method }, callback);
-    });
-  }
+function injectedMethod(tab, method, callback) {
+  chrome.tabs.executeScript(tab.id, { file: "inject.js" }, function () {
+    chrome.tabs.sendMessage(tab.id, { method: method }, callback);
+  });
+}
 
-  function injectedMethod2 (tab, bgColor1, bgColor2, method, callback) {
-    console.log(bgColor1 + " " + bgColor2);
-    chrome.tabs.executeScript(tab.id, {code: 'var bgColor1 = ' + JSON.stringify(bgColor1) + ';' + 
-                                             'var bgColor2 = ' + JSON.stringify(bgColor2) + ';'}, function() {
-      chrome.tabs.executeScript(tab.id, { file: 'elements.js' }, function(){
+function injectedMethod2(tab, bgColor1, bgColor2, method, callback) {
+  console.log(bgColor1 + " " + bgColor2);
+  chrome.tabs.executeScript(
+    tab.id,
+    {
+      code:
+        "var bgColor1 = " +
+        JSON.stringify(bgColor1) +
+        ";" +
+        "var bgColor2 = " +
+        JSON.stringify(bgColor2) +
+        ";",
+    },
+    function () {
+      chrome.tabs.executeScript(tab.id, { file: "elements.js" }, function () {
         chrome.tabs.sendMessage(tab.id, { method: method }, callback);
       });
-    })
-    
-  }
-  
+    }
+  );
+}
+
 //   function getBgColors (tab) {
 //     // When we get a result back from the getBgColors
 //     // method, alert the data
@@ -26,61 +36,60 @@ function injectedMethod (tab, method, callback) {
 //     });
 //   }
 
-  // Get background-color values from the current tab
+// Get background-color values from the current tab
 // and open them in Colorpeek.
-function getBgColors (tab) {
-    injectedMethod(tab, 'getBgColors', function (response) {
-      var colors = response.data;
-      if (colors && colors.length) {
-        //alert('Colors on page: ' + colors);
-        console.log(colors);
-        //return colors;
-        chrome.runtime.sendMessage({
-            msg: "completed", 
-            data: {
-                subject: "colors",
-                content: colors
-            }
-        });
-        //sendResponse({colors: colors});
-      } else {
-        alert('No background colors were found! :(');
-      }
+function getBgColors(tab) {
+  injectedMethod(tab, "getBgColors", function (response) {
+    var colors = response.data;
+    if (colors && colors.length) {
+      //alert('Colors on page: ' + colors);
+      console.log(colors);
+      //return colors;
+      chrome.runtime.sendMessage({
+        msg: "completed",
+        data: {
+          subject: "colors",
+          content: colors,
+        },
+      });
+      //sendResponse({colors: colors});
+    } else {
+      alert("No background colors were found! :(");
+    }
     //eturn true;
-    })
-  }
+  });
+}
 
 function getDomElements(tab, bgColor1, bgColor2) {
   //console.log(document.getElementsByTagName('*'));
-  injectedMethod2(tab, bgColor1, bgColor2, 'getElements', function (response) {
+  injectedMethod2(tab, bgColor1, bgColor2, "getElements", function (response) {
     console.log(response);
-  //eturn true;
-  })
-
+    //eturn true;
+  });
 }
-  
-  // When the browser action is clicked, call the
-  // getBgColors function.
+
+// When the browser action is clicked, call the
+// getBgColors function.
 //chrome.browserAction.onClicked.addListener(getBgColors);
-chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
-    console.log(request);
-    if (request.from === "popup" && request.subject === "DOMInfo") {
-        getBgColors(request.tab); 
-        //sendResponse({colors: getBgColors(request.tab)});
-    }
-    if ((request.from === 'popup') && (request.subject === 'Elements')) {
-      console.log("here");
-      getDomElements(request.tab, request.bgColor1, request.bgColor2);
-    }
+chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+  console.log(request);
+  if (request.from === "popup" && request.subject === "DOMInfo") {
+    getBgColors(request.tab);
+    //sendResponse({colors: getBgColors(request.tab)});
+  }
+  if (request.from === "popup" && request.subject === "Elements") {
+    console.log("here");
+    getDomElements(request.tab, request.bgColor1, request.bgColor2);
+  }
 });
 //       console.log("here");
 //       if (request.from === "popup" && request.subject === "DOMInfo") {
-//         ///var colors =  
+//         ///var colors =
 //        // sendResponse({colors: })
 //       }
 //   })
 //   chrome.runtime.sendMessage({
-//     msg: "completed", 
+//     msg: "completed",
 //     data: {
 //         subject: "colors",
 //         content: getBgColors
--- popup.js
+++ popup.js
@@ -1,125 +1,150 @@
-
 console.log("popup!");
 var colors = {};
 var query = { active: true, currentWindow: true };
 chrome.tabs.query(query, callback);
 
 function callback(tabs) {
-    var currentTab = tabs[0]; // there will be only one in this array
-    console.log(currentTab); // also has properties like currentTab.id
-    chrome.runtime.sendMessage({tab: currentTab, from: 'popup', subject: 'DOMInfo'});
-  }
+  var currentTab = tabs[0]; // there will be only one in this array
+  console.log(currentTab); // also has properties like currentTab.id
+  chrome.runtime.sendMessage({
+    tab: currentTab,
+    from: "popup",
+    subject: "DOMInfo",
+  });
+}
 
-chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
-    console.log(request.data.content); // these are the color codes!!!
+chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+  console.log(request.data.content); // these are the color codes!!!
 
-    // build the html
-    const backgroundButtons = document.getElementsByClassName("backgroundButton");
-    const bgbuttonArr = request.data.content;
+  // build the html
+  const backgroundButtons = document.getElementsByClassName("backgroundButton");
+  const bgbuttonArr = request.data.content;
 
-    bgbuttonArr.forEach((color, i) => {
-        let btn = document.createElement('button');
-        btn.className = 'backgroundButton';
-        btn.id = `backgroundButton_${i}`
+  bgbuttonArr.forEach((color, i) => {
+    let btn = document.createElement("button");
+    btn.className = "backgroundButton";
+    btn.id = `backgroundButton_${i}`;
+    btn.style.backgroundColor = color;
+
+    $(btn).spectrum({
+      preferredFormat: "hex",
+      change: function (color) {
+        var ind = parseInt(this.id.substring(3, 4));
+        var idi = "#cpi" + ind;
+        var ido = "#cpo" + ind;
+        var temp =
+          "rgb(" +
+          Math.round(color._r) +
+          ", " +
+          Math.round(color._g) +
+          ", " +
+          Math.round(color._b) +
+          ")";
+        console.log(temp);
+        color = color.toString().toUpperCase();
+        $(ido).val(color);
+        var params = {}; // use params to update the background color of this button, cache original values
+        //var colors = {}; // key is what it was, value is what it is changed to
+        var bgColor1 = btn.style.backgroundColor;
+        var bgColor2 = temp;
+        colors[temp] = btn.style.backgroundColor;
         btn.style.backgroundColor = color;
+        //params[parseHEX($(idi).val())] = parseHEX(color);
+        console.log(color);
 
-        $(btn).spectrum({
-          preferredFormat: "hex",
-          change: function (color) {
-            var ind = parseInt(this.id.substring(3, 4));
-            var idi = "#cpi" + ind;
-            var ido = "#cpo" + ind;
-            var temp = "rgb(" + Math.round(color._r) + ", " + Math.round(color._g) + ", " + Math.round(color._b) + ")";
-            console.log(temp);
-            color = color.toString().toUpperCase();
-            $(ido).val(color);
-            var params = {}; // use params to update the background color of this button, cache original values
-            //var colors = {}; // key is what it was, value is what it is changed to
-            var bgColor1 = btn.style.backgroundColor;
-            var bgColor2 = temp;
-            colors[temp] = btn.style.backgroundColor;
-            btn.style.backgroundColor = color;
-            //params[parseHEX($(idi).val())] = parseHEX(color);
-            console.log(color); 
+        // send color back
+        chrome.tabs.query({ active: true, currentWindow: true }, function (
+          tabs
+        ) {
+          var query = { active: true, currentWindow: true };
+          chrome.tabs.query(query, callback1);
 
-            // send color back
-            chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-
-              var query = { active: true, currentWindow: true };
-              chrome.tabs.query(query, callback1);
-
-              function callback1(tabs) {
-                  var currentTab = tabs[0]; // there will be only one in this array
-                  console.log(currentTab); // also has properties like currentTab.id
-                  chrome.runtime.sendMessage({tab: currentTab, from: 'popup', subject: 'Elements', bgColor1: bgColor1, bgColor2: bgColor2}, function(response) {
-                    console.log(response);
-                  });
-                }
-            });
-
+          function callback1(tabs) {
+            var currentTab = tabs[0]; // there will be only one in this array
+            console.log(currentTab); // also has properties like currentTab.id
+            chrome.runtime.sendMessage(
+              {
+                tab: currentTab,
+                from: "popup",
+                subject: "Elements",
+                bgColor1: bgColor1,
+                bgColor2: bgColor2,
+              },
+              function (response) {
+                console.log(response);
+              }
+            );
           }
         });
-      
-        let changeBackgroundDiv = document.getElementById('changeBackground');
-        changeBackgroundDiv.appendChild(btn);
-      });
+      },
+    });
 
-      for (let backgroundButton of backgroundButtons) {
-        backgroundButton.addEventListener('click', (element) => {
-          // let bgColor = element.target.style.backgroundColor;
-          //console.log(backgroundButton.style.backgroundColor);
-          //let bgColor1 = element.target.style.backgroundColor;
-          //let bgColor2 = colors[element.target.style.backgroundColor];
-          //console.log("Color 1 = " + bgColor1 + " Color 2 = " + bgColor2);
-          /***Used to run a one line query on webpage***/
-          chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+    let changeBackgroundDiv = document.getElementById("changeBackground");
+    changeBackgroundDiv.appendChild(btn);
+  });
 
-            function modifyDOM() {
-              var query = { active: true, currentWindow: true };
-              chrome.tabs.query(query, callback1);
+  for (let backgroundButton of backgroundButtons) {
+    backgroundButton.addEventListener("click", (element) => {
+      // let bgColor = element.target.style.backgroundColor;
+      //console.log(backgroundButton.style.backgroundColor);
+      //let bgColor1 = element.target.style.backgroundColor;
+      //let bgColor2 = colors[element.target.style.backgroundColor];
+      //console.log("Color 1 = " + bgColor1 + " Color 2 = " + bgColor2);
+      /***Used to run a one line query on webpage***/
+      chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+        function modifyDOM() {
+          var query = { active: true, currentWindow: true };
+          chrome.tabs.query(query, callback1);
 
-              function callback1(tabs) {
-                  var currentTab = tabs[0]; // there will be only one in this array
-                  console.log(currentTab); // also has properties like currentTab.id
-                  chrome.runtime.sendMessage({tab: currentTab, from: 'popup', subject: 'Elements', bgColor: bgColor}, function(response) {
-                    console.log(response);
-                  });
-                }
-              // chrome.runtime.sendMessage({subject:'Elements'}, function(response) {
-              //   console.log(response);
-              // })
-
-              // chrome.tabs.getSelected(null, function(tab) {
-              //   // Send a request to the content script.
-              //   chrome.tabs.sendRequest(tab.id, {action: "getDOM"}, function(response) {
-              //     console.log(response.dom);
-              //   });
-              // });
-                //You can play with your DOM here or check URL against your regex
-                //console.log('Tab script:');
-                // console.log(document.body.innerHTML);
-                //return document.body.querySelectorAll('*'); // NEED TO DO THIS IN A NEW CONTENT SCRIPT https://stackoverflow.com/questions/1964225/accessing-current-tab-dom-object-from-popup-html
-            }
-
-            function getColor(bgColor) {
-              console.log("here");
-              var nodes = document.getElementsByTageName('*');
-              var bgs = [];
-              for (var node of nodes) {
-                  bgs.push(node.style.backgroundColor);
-                  if (node.style.backgroundColor == bgColor) {
-                      node.style.backgroundColor = rgb(0,0,0);
-                  }
+          function callback1(tabs) {
+            var currentTab = tabs[0]; // there will be only one in this array
+            console.log(currentTab); // also has properties like currentTab.id
+            chrome.runtime.sendMessage(
+              {
+                tab: currentTab,
+                from: "popup",
+                subject: "Elements",
+                bgColor: bgColor,
+              },
+              function (response) {
+                console.log(response);
               }
-              return bgs;
+            );
+          }
+          // chrome.runtime.sendMessage({subject:'Elements'}, function(response) {
+          //   console.log(response);
+          // })
 
+          // chrome.tabs.getSelected(null, function(tab) {
+          //   // Send a request to the content script.
+          //   chrome.tabs.sendRequest(tab.id, {action: "getDOM"}, function(response) {
+          //     console.log(response.dom);
+          //   });
+          // });
+          //You can play with your DOM here or check URL against your regex
+          //console.log('Tab script:');
+          // console.log(document.body.innerHTML);
+          //return document.body.querySelectorAll('*'); // NEED TO DO THIS IN A NEW CONTENT SCRIPT https://stackoverflow.com/questions/1964225/accessing-current-tab-dom-object-from-popup-html
+        }
+
+        function getColor(bgColor) {
+          console.log("here");
+          var nodes = document.getElementsByTageName("*");
+          var bgs = [];
+          for (var node of nodes) {
+            bgs.push(node.style.backgroundColor);
+            if (node.style.backgroundColor == bgColor) {
+              node.style.backgroundColor = rgb(0, 0, 0);
             }
-            modifyDOM();
-            // chrome.tabs.executeScript(tab.id, { file: 'inject.js' }, function(){
-            //   chrome.tabs.sendMessage(tab.id, { method: method }, callback);
-            // });
+          }
+          return bgs;
+        }
+        modifyDOM();
+        // chrome.tabs.executeScript(tab.id, { file: 'inject.js' }, function(){
+        //   chrome.tabs.sendMessage(tab.id, { method: method }, callback);
+        // });
 
-           /* chrome.tabs.executeScript(tabs[0].id, {
+        /* chrome.tabs.executeScript(tabs[0].id, {
               //code: '(' + modifyDOM + ')();'
               // changes background color to black of selected elements
               // code: `var nodes = document.getElementsByTagName('*');
@@ -174,16 +199,13 @@ chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
                 //console.log(results[0]);
 
         });*/
-            // chrome.tabs.executeScript(tabs[0].id, {
-            //     code: '(' + modifyDOM + ')();'}, 
-            //     function(results){
-            //         console.log('Popup script:')
-            //         console.log(results[0]);
-            //     });
-
-          });
-        });
-      };
+        // chrome.tabs.executeScript(tabs[0].id, {
+        //     code: '(' + modifyDOM + ')();'},
+        //     function(results){
+        //         console.log('Popup script:')
+        //         console.log(results[0]);
+        //     });
+      });
+    });
+  }
 });
-
-
